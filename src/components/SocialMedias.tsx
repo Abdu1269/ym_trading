38b7b0d3717e9b3ba99c88b489bbad997632d9ad
Abdu1269 <50406895+Abdu1269@users.noreply.github.com>
2024-08-28 00:26:09 +0300

    fix: new

--- src/components/SocialMedias.tsx
+++ src/components/SocialMedias.tsx
@@ -1,24 +1,21 @@
 "use client";
-import React from "react";
-import useSWR from "swr";
-import fetcher from "@/shared/utils/fetcher";
-import {
-  FaInstagram,
-  FaYoutubeSquare,
-  FaLinkedinIn,
-  FaFacebookMessenger,
-} from "react-icons/fa";
-import Link from "next/link";
-import { ConvertToExternal } from "@/shared/utils/externalLink/convertToExternal";
-import { FaX } from "react-icons/fa6";
 import { CollectionQuery } from "@/model/collection.model";
 import { socialMedia } from "@/model/socialMedias.model";
-import ErrorPage from "./ErrorPage";
-import LoadingPage from "./LoadingPage";
+import { ConvertToExternal } from "@/shared/utils/externalLink/convertToExternal";
+import fetcher from "@/shared/utils/fetcher";
+import Link from "next/link";
+// import {
+//   FaFacebookMessenger,
+//   FaInstagram,
+//   FaLinkedinIn,
+//   FaYoutubeSquare,
+// } from "react-icons/fa";
+// import { FaX } from "react-icons/fa6";
+import useSWR from "swr";
 
 const SocialMedias = () => {
   const collection: CollectionQuery = {
-    locale : 'en',
+    locale: "en",
     orderBy: [
       {
         direction: "desc",
@@ -40,27 +37,26 @@ const SocialMedias = () => {
 
   if (isLoading) {
     return;
-   <div> </div>
+    <div> </div>;
   }
 
   if (error) {
     return;
-    <div> </div>
+    <div> </div>;
   }
-
 
   console.log(socialMedia?.data[0]);
   return (
     <div className="mt-10 content-center flex justify-center items-center  gap-10 ">
       {socialMedia?.data?.map((socialMedia: socialMedia) => (
         <div key={socialMedia.id} className="group">
-          <Link href={ConvertToExternal(socialMedia.link)} target="_blank">
+          {/* <Link href={ConvertToExternal(socialMedia.link)} target="_blank">
             {socialMedia.name.en === "Facebook" && <FaFacebookMessenger />}
             {socialMedia.name.en === "Twitter" && <FaX />}
             {socialMedia.name.en === "LinkedIn" && <FaLinkedinIn />}
             {socialMedia.name.en === "Youtube" && <FaYoutubeSquare />}
             {socialMedia.name.en === "Instagram" && <FaInstagram />}
-          </Link>
+          </Link> */}
         </div>
       ))}
     </div>
