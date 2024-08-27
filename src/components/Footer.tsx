d0894fd5014c50450b303d93a9d15cc9e4d91d66
Abdu1269 <50406895+Abdu1269@users.noreply.github.com>
2024-08-28 00:34:48 +0300

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,10 +9,15 @@ const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-yellow-700 to-emerald-900 text-sky-900 p-6 lg:p-10">
       <div className="container mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center space-y-8 lg:space-y-0">
-        
         {/* Logo and Company Info */}
         <div className="flex flex-col items-center lg:items-start space-y-4 lg:space-y-2">
-          <Image src={logo} alt="Logo" width={150} height={150} className="mb-4" />
+          <Image
+            src={logo}
+            alt="Logo"
+            width={150}
+            height={150}
+            className="mb-4"
+          />
           <p className="text-xl text-neutral-950 font-semibold text-center lg:text-left">
             YM TRADING PLC
             <br />
@@ -20,11 +25,13 @@ const Footer = () => {
           </p>
           <SocialMedias />
         </div>
-        
+
         {/* Navigation Links */}
         <div className="flex flex-col lg:flex-row lg:space-x-12">
           <nav className="flex flex-col text-white space-y-4 lg:space-y-2">
-            <h6 className="text-lg  font-semibold text-neutral-100">Services</h6>
+            <h6 className="text-lg  font-semibold text-neutral-100">
+              Services
+            </h6>
             <a className="link link-hover">Import</a>
             <a className="link link-hover">Export</a>
             <a className="link link-hover">Commission Works</a>
@@ -32,8 +39,12 @@ const Footer = () => {
 
           <nav className="flex flex-col text-white space-y-4 lg:space-y-2">
             <h6 className="text-lg font-semibold text-neutral-100">Company</h6>
-            <a className="link link-hover" href="/about">About Us</a>
-            <a className="link link-hover"href="#contact-us">Contact</a>
+            <a className="link link-hover" href="/about">
+              About Us
+            </a>
+            <a className="link link-hover" href="#contact-us">
+              Contact
+            </a>
           </nav>
 
           <nav className="flex flex-col text-white space-y-4 lg:space-y-2">
@@ -42,7 +53,7 @@ const Footer = () => {
             <a className="link link-hover">Privacy Policy</a>
           </nav>
         </div>
-        
+
         {/* Rating Section */}
         <div className="flex flex-col items-center space-y-4">
           <div className="rating rating-lg">
@@ -72,11 +83,13 @@ const Footer = () => {
               aria-label="rating-4"
             />
             <input
+              title="rating"
               type="radio"
               name="rating-9"
               className="mask mask-star-2"
             />
             <input
+              title="rating"
               type="radio"
               name="rating-9"
               className="mask mask-star-2"
--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar/navbar";
-import Footer from "./Footer/footer";
+import Footer from "@/components/Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Navbar />
-      <main className="h-screen">{children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
